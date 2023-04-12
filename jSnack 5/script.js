105281e4ad5432a344e2319141d51a51e6de87a5
MikePio <michelepiopilla@gmail.com>
2023-04-13 01:29:38 +0200

    completed jsnack 5

--- jSnack 5/script.js
+++ jSnack 5/script.js
@@ -5,7 +5,28 @@ Ogni oggetto descriverà una bici da corsa con le seguenti proprietà : nome e p
 Stampare in console la bici con peso minore utilizzando destructuring e template literal
 */
 
+const biciclette = [
+    {nome: "Bici A", peso: 11},
+    {nome: "Bici B", peso: 9},
+    {nome: "Bici C", peso: 7},
+    {nome: "Bici D", peso: 10},
+    {nome: "Bici E", peso: 12},
+];
 
+//*forEach 
+let biciLeggera = biciclette[0];
+biciclette.forEach(bici => {
+    if (bici.peso < biciLeggera.peso) {
+        biciLeggera = bici;
+    }
+});
+
+console.log(`La bici più leggera è ${biciLeggera.nome} con un peso di ${biciLeggera.peso} kg`);
+
+//*filter [migliore in questo caso]
+biciLeggera = biciclette.filter(bici => bici.peso === Math.min(...biciclette.map(bici => bici.peso)))[0];
+
+console.log(`La bici più leggera è ${biciLeggera.nome} con un peso di ${biciLeggera.peso} kg`);
 
 
 
